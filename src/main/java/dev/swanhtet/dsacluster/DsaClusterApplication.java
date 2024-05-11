a7fb7e3bdc5f0fd564b9f5d2a10b06dfd1173336
SwanHtetMorgan <swanhtet102002@gmail.com>
2024-05-11 21:03:25 +0200

    adding file

---
+++ src/main/java/dev/swanhtet/dsacluster/DsaClusterApplication.java
@@ -0,0 +1,14 @@
+package dev.swanhtet.dsacluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaClusterApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DsaClusterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/swanhtet/dsacluster/DsaClusterApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.swanhtet.dsacluster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaClusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
